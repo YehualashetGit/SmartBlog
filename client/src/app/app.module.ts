6cde0a014d5089c14c1251719b5e14abd21633ab
yehualashet <yehuala5355@gmail.com>
2017-10-26 14:28:13 +0300

    Set up some route

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+const appRoute:Routes=[
+  { path: '', component: HomeComponent },
+  {path: 'dashboard',component:DashboardComponent},
+  { path: '**', component: HomeComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
