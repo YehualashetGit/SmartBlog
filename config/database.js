38d90deda7f71825970ca02c51d7e539494f454e
yehualashet <yehuala5355@gmail.com>
2017-11-01 17:56:12 +0300

    Updating gitignore file

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
+  const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 module.exports={
     uri:'mongodb://localhost/smart-blog',
     secret:crypto,
